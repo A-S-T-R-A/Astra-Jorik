e9af24ea54035a31300f216e16d7b05159bf4014
Art1us <109896229+Art1us@users.noreply.github.com>
2023-04-02 21:08:35 +0300

    fin

--- src/widgets/HomeProjects/HomeProjects.tsx
+++ src/widgets/HomeProjects/HomeProjects.tsx
@@ -5,30 +5,18 @@ import { Epigraph } from "shared/ui/Epigraph/Epigraph"
 import { Typography, TypographyColor } from "shared/ui/Typography/Typography"
 import { ProjectsRow } from "./ProjectsRow/ProjectsRow"
 import { classNames } from "shared/lib/classNames"
+import { ProjectLink } from "./ProjectLink/ProjectLink"
 
 export function HomeProjects() {
     return (
         <div className={styles.wrapper}>
             <SectionTitle title="Recent Project" epigraph="Check out" className={styles.title} />
 
-            <ProjectLink className={styles.link} />
+            <ProjectLink className={styles.link} color={TypographyColor.DARK_GRAY}>
+                View all projects
+            </ProjectLink>
 
             <ProjectsRow className={styles.projects} />
-        </div>
-    )
-}
-
-interface ProjectLink {
-    className?: string
-}
-
-function ProjectLink({ className }: ProjectLink) {
-    return (
-        <div className={classNames(styles.moreProjects, {}, [className])}>
-            <Typography color={TypographyColor.DARK_GRAY} className={styles.moreProjectsText}>
-                view more projects
-            </Typography>
-            <div className={styles.arrowRight} />
         </div>
     )
 }
--- src/widgets/HomeProjects/ProjectsRow/ProjectsRow.tsx
+++ src/widgets/HomeProjects/ProjectsRow/ProjectsRow.tsx
@@ -1,10 +1,67 @@
 import { classNames } from "shared/lib/classNames"
 import styles from "./ProjectsRow.module.css"
+import "./ProjectRow.css"
+import projImg from "./proj.jpg"
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Navigation, Pagination } from "swiper"
+import "swiper/css"
+import "swiper/css/navigation"
+import "swiper/css/pagination"
+import "swiper/css/scrollbar"
+import { Typography, TypographyColor, TypographyVariant } from "shared/ui/Typography/Typography"
+import { ProjectLink } from "../ProjectLink/ProjectLink"
 
 interface ProjectsRow {
     className?: string
 }
 
 export function ProjectsRow({ className }: ProjectsRow) {
-    return <div className={classNames(styles.container, {}, [className])}>ProjectsRow</div>
+    return (
+        <div className={classNames(styles.container, {}, [className, "projects-swiper-container"])}>
+            <Swiper
+                slidesPerView={1}
+                className={styles.swiper}
+                pagination={{ clickable: true }}
+                modules={[Pagination, Navigation]}
+                spaceBetween={30}
+                breakpoints={{
+                    620: {
+                        slidesPerView: 2,
+                    },
+                    920: {
+                        slidesPerView: 3,
+                    },
+                    1120: {
+                        slidesPerView: 4,
+                    },
+                    1420: {
+                        slidesPerView: 5,
+                    },
+                }}
+            >
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((proj, index) => (
+                    <SwiperSlide key={index} className={styles.slide}>
+                        <img src={projImg} alt="" className={styles.img} />
+                        <div className={styles.infoOverlay}>
+                            <div className={styles.slideInfo}>
+                                <Typography
+                                    variant={TypographyVariant.H4}
+                                    color={TypographyColor.INVERTED}
+                                    className={styles.slideText}
+                                >
+                                    Best First Project
+                                </Typography>
+                                <ProjectLink
+                                    color={TypographyColor.DARK_GRAY}
+                                    variant={TypographyVariant.SMALL}
+                                >
+                                    view project
+                                </ProjectLink>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
+    )
 }
