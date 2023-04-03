66f7751cd311c7f1d2c323c4c38a27356348a73d
Art1us <109896229+Art1us@users.noreply.github.com>
2023-04-03 18:23:56 +0300

    deleted unused

--- src/shared/constants/contactUs/index.ts
+++
@@ -1,6 +0,0 @@
-import reactNativeIcon from "./mobile-development.png"
-import webDesignIcon from "./web-design.png"
-import frontendIcon from "./data.png"
-import backendIcon from "./development.png"
-
-export { reactNativeIcon, webDesignIcon, frontendIcon, backendIcon }
