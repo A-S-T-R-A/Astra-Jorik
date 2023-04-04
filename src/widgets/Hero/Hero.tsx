f3c706d37a6486d50bc9ca10b49d3a97953697a5
Art1us <109896229+Art1us@users.noreply.github.com>
2023-04-04 14:37:22 +0300

    fin

--- src/widgets/Hero/Hero.tsx
+++ src/widgets/Hero/Hero.tsx
@@ -1,11 +1,10 @@
 import { heroImg } from "shared/constants/home"
-import { Section } from "shared/ui/Section/Section"
 import styles from "./Hero.module.css"
 
 export function Hero() {
     return (
-        <Section>
+        <div className={styles.wrapper}>
             <img src={heroImg} alt="hero banner" className={styles.img} />
-        </Section>
+        </div>
     )
 }
